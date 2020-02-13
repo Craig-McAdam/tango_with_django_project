6225d5ca8222fd3ddeacec2612033a4b82005146
Craig McAdam <2314209M@student.gla.ac.uk>
2020-02-13 18:17:10 +0000

    10.4 A Cookie Tasting Session

--- rango/views.py
+++ rango/views.py
@@ -12,27 +12,23 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Render the response and send it back!
-    # We make use of the shortcut function to make our lives easier
-    # Note that the first parameter is the template we wish to use.
+    request.session.set_test_cookie()
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     return render(request,'rango/about.html')
 
 def show_category(request, category_name_slug):
